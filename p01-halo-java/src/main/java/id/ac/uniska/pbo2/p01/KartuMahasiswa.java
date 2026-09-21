1543d109ed06fd85cf9319809c128728d00d3352
Elvira Eri Saputri <hipieepi@gmail.com>
2026-09-21 19:52:08 +0800

    Menambahkan Class KartuMahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author Asus
+ */
+public class KartuMahasiswa {
+    public static void main(String[] args) {
+        String nama = "Elvira Eri Saputri";
+        String npm = "241001461";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin belajar lebih dalam tentang java";
+
+        System.out.println("==============================");
+        System.out.println(" KARTU MAHASISWA PBO 2");
+        System.out.println("==============================");
+        System.out.println("Nama    : " + nama);
+        System.out.println("NPM     : " + npm);
+        System.out.println("Prodi   : " + prodi);
+        System.out.println("Semester: " + semester);
+        System.out.println("Alasan  : " + alasan);
+        System.out.println("==============================");
+    }
+}
